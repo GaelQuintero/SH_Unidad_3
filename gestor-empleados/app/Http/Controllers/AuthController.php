abad1736f45871ba122b498108eab373cd4b92d7
GaelQuintero <tober223@outlook.com>
2024-11-16 01:29:26 -0600

    Se agrego el back-end para que el usuario inicie sesión o se pueda registrar

--- gestor-empleados/app/Http/Controllers/AuthController.php
+++ gestor-empleados/app/Http/Controllers/AuthController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -14,8 +16,33 @@ class AuthController extends Controller
         return view("modules/auth/registro-empleado");
     }
 
-    public function registrarEmpleado (){
-        return view("modules/auth/registro-empleado");
+    public function registrarEmpleado(Request $request) {
+        $item = new User();
+        $item->name = $request->name;
+        $item->email = $request->email;
+        $item->password = Hash::make($request->password);
+        $item->save();
+
+        return redirect()->route('login-empleado')->with('success', 'Te has registrado correctamente');
     }
+
+    public function login(Request $request){
+       $credenciales = [
+           'email' => $request->email,
+           'password' => $request->password
+       ];
+
+       if (Auth::attempt($credenciales)) {
+        return redirect()->route('dashboard')->with('success', 'Haz iniciado sesión correctamente');
+       } else{
+        return redirect()->route('login-empleado')->with('error', 'Verifique sus datos');
+       }
+    }
+
+    public function dashboard (){
+        return view("modules/home/dashboard");
+    }
+
+
 }
 
--- gestor-empleados/routes/web.php
+++ gestor-empleados/routes/web.php
@@ -4,5 +4,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthController::class, 'index'])->name('login-empleado');
+
 Route::get('/registro-empleado',[AuthController::class, 'registroEmpleado'])->name('registro-empleado');
-Route::post('/registrar-empleado', [AuthController::class, 'registrar-empleado'])->name('registrar-empleado');
+
+Route::post('/registrar-empleado', [AuthController::class, 'registrarEmpleado'])->name('registrar-empleado');
+
+Route::post('/login', [AuthController::class,'login'])->name('login');
+
+Route::get('/dashboard', [AuthController::class,'dashboard'])->name('dashboard');
