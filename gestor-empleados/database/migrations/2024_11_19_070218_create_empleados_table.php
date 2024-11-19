f6934729141fd15ae145ffbd6af66138e722c6b4
Sweet_Jane <113494008+FatimaVA@users.noreply.github.com>
2024-11-19 01:36:32 -0600

    Migración tabla empleados

---
+++ gestor-empleados/database/migrations/2024_11_19_070218_create_empleados_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('idEmpleado');
+            $table->string('Nombre', 100);
+            $table->string('Apellido', 100);
+            $table->string('NumeroEmpleado', 50)->unique();
+            $table->timestamp('FechaRegistrado')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
