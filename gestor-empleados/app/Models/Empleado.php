fd2d00eed7b2f6af9c7d95742da40fb3f4710db3
Sweet_Jane <113494008+FatimaVA@users.noreply.github.com>
2024-11-19 01:51:17 -0600

    modelo empleado

---
+++ gestor-empleados/app/Models/Empleado.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'idEmpleado'; // Cambia la llave primaria
+    public $timestamps = true; // Habilita las marcas de tiempo
+
+    protected $fillable = [
+        'Nombre',
+        'Apellido',
+        'NumeroEmpleado',
+        'FechaRegistrado',
+    ];
+}
